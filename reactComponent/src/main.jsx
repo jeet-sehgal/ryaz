460fa82030cd73ead4c9e8aa18d9ebe2d7ca1807
jeet sehgal <jeetsehgal786@gmail.com>
2024-09-20 20:14:31 +0530

    counter

--- reactComponent/src/main.jsx
+++ reactComponent/src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-const ele=(<a href="https://google.com" target='_blank'>click</a>)
+const ele=React.createElement("h1",{href:"ffs"},"hi my name is jeet")
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
